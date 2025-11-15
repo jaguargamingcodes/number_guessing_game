7dc3a52f6650b51756a76349b92079138481d457
emad <Emad Mirza>
2025-11-15 15:10:45 -0500

    Version 2.0 update

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-
+bestScore = 0;
 streak = 0;
 function start() {
     RightRandomNumber = Math.floor(Math.random() * 100)
@@ -23,10 +23,16 @@ function start() {
 function inc() {
     streak++;
     document.getElementById("xp").innerHTML = streak;
+    if(document.getElementById('highScore').innerHTML == 0 || document.getElementById('highScore').innerHTML < streak ) {
+        bestScore = streak;
+        document.getElementById('highScore').innerHTML = bestScore;
+        console.log(streak)
+    }
 }
 
 function lose() {
-    document.getElementById("xp").innerHTML = "0";
+    streak = 0;
+    document.getElementById("xp").innerHTML = streak;
 }
 
 function f1() {
@@ -58,3 +64,4 @@ function f3() {
         window.alert("YOU DIED!!!");
     }
 }
+
